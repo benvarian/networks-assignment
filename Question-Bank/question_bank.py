f33dcb910a570451c466e979f810e5ec9c6e4510
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-05-01 16:13:09 +0800

    new formatting

--- Question-Bank/question_bank.py
+++ Question-Bank/question_bank.py
@@ -1,8 +1,8 @@
 import random
 from get_questions import *
 
-questions = get_questions('Question-Bank/QuestionCSV/Maths.csv')
-
-sample_questions = random.sample(questions, 10)
-
-print(sample_questions)
+def sample_questions(type, num):
+    questions = get_questions(type)
+    return random.sample(questions, num)
+    
+print(sample_questions('Maths', 10))
