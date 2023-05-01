9151c4b3a16eccf605fd33a19799ba2d1d375573
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-05-01 15:33:14 +0800

    random sampling of questions

--- Question-Bank/question_bank.py
+++ Question-Bank/question_bank.py
@@ -1,2 +1,8 @@
+import random
 from get_questions import *
 
+questions = get_questions('Question-Bank/QuestionCSV/Maths.csv')
+
+sample_questions = random.sample(questions, 10)
+
+print(sample_questions)
