ddbc6320b9f6c7e6de93320dfdd32a315cd47963
B00mStream <57031880+B00mStream@users.noreply.github.com>
2023-05-02 23:44:21 +0800

    Randomizing questions prototyping
    still working on both the randomizing question and the getting answer functions, but the randomizing kinda works i guess

--- Question-Bank/get_questions.py
+++ Question-Bank/get_questions.py
@@ -1,4 +1,6 @@
 import os
+from re import findall
+from random import randint
 
 # Opens csv file and handles errors
 # Returns a dictionary of questions, or None if file not found
@@ -20,13 +22,15 @@ def get_questions(filePath):
                 except ValueError:
                     continue
                 if row[0] in questions:
-                        continue
+                    continue
                 else:
                     # Replace \\n with \n because when \n is stored in the csv file
                     # it is changed to \\n when read in by Python, this will help 
                     # question/answer formatting
                     row[1] = row[1].replace("\\n", "\n")
+                    row[1] = row[1].replace("\\t", "\t")
                     row[2] = row[2].replace("\\n", "\n")
+                    row[2] = row[2].replace("\\t", "\t")
                     questions[row[0]] = [row[1], row[2]]
             return questions
     # File Path not found
@@ -34,8 +38,38 @@ def get_questions(filePath):
         print(f"""Filepath "{filePath}" not found""")
         return None
 
-# # TESTING
-# if __name__ == "__main__":
-#     pythonDict = getQuestions("QuestionCSV\QuestionsP.csv")
-#     for key in pythonDict:
-#         print(f"Question ID: {key}\nQuestion: {pythonDict[key][0]}\nAnswer: {pythonDict[key][1]}\n")
+# WORK IN PROGRESS
+# Given a generic question string it will randomize
+# each variable stored in curly brackets and return
+# the question with the placeholders replaced with values
+def randomize_question(questionString):
+    dict = {}
+    # credit to chatgpt for the regex to capture the values inside {} whilst negating double brackets {{}}
+    values = findall(r"\{([^{}\d]*[^{}]*?)\}", f"{questionString}")
+    for i in values:
+        if i not in dict:
+            dict[i] = randint(1,100) # HARDCODED ALL VARIABLES TO BE WITHIN 1 AND 100 FOR NOW -- NEED TO REVIST LATER
+            questionString = questionString.replace(f"{{{i}}}", f"{dict[i]}")
+    return questionString
+    
+    
+
+# WORK IN PROGRESS
+# Given a question string it writes to a tmp file
+# then executes the string, and returns the answer
+# deletes the tmp file afterwards
+def get_answer(questionNum, questionString):
+    # if question number is odd it is a python question
+    if questionNum % 2 == 1:
+        return None
+    # else treat it like a C question
+    else:
+        return None
+
+# TESTING
+if __name__ == "__main__":
+    pythonDict = get_questions("Question-Bank\QuestionCSV\QuestionsP.csv")
+    for question in pythonDict:
+        pythonDict[question][0] = randomize_question(pythonDict[question][0])
+    for key in pythonDict:
+        print(f"Question ID: {key}\nQuestion: {pythonDict[key][0]}\n\nAnswer: {pythonDict[key][1]}\n")
