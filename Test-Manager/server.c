14abd22d329c83722ae2fffd74d80454c21f8ba8
benvarian <88700858+benvarian@users.noreply.github.com>
2023-05-06 20:04:42 +0800

    up and running baby

--- Test-Manager/server.c
+++ Test-Manager/server.c
@@ -17,12 +17,57 @@
 #include <signal.h>
 
 #define BACKLOG 10
-#define MAXDATASIZE 100
+#define MAXDATASIZE 2047
+#define BSIZE 1024
+#define SOCKET int
 
 void usage()
 {
     fprintf(stderr, "usage: ./server port\n");
     exit(EXIT_FAILURE);
+}
+
+void drop_client(SOCKET socket)
+{
+    close(socket);
+}
+
+const char *get_content_type(const char *path)
+{
+    const char *last_dot = strrchr(path, '.');
+    if (last_dot)
+    {
+        if (strcmp(last_dot, ".css") == 0)
+            return "text/css";
+        if (strcmp(last_dot, ".csv") == 0)
+            return "text/csv";
+        if (strcmp(last_dot, ".gif") == 0)
+            return "image/gif";
+        if (strcmp(last_dot, ".htm") == 0)
+            return "text/html";
+        if (strcmp(last_dot, ".html") == 0)
+            return "text/html";
+        if (strcmp(last_dot, ".ico") == 0)
+            return "image/x-icon";
+        if (strcmp(last_dot, ".jpeg") == 0)
+            return "image/jpeg";
+        if (strcmp(last_dot, ".jpg") == 0)
+            return "image/jpeg";
+        if (strcmp(last_dot, ".js") == 0)
+            return "application/javascript";
+        if (strcmp(last_dot, ".json") == 0)
+            return "application/json";
+        if (strcmp(last_dot, ".png") == 0)
+            return "image/png";
+        if (strcmp(last_dot, ".pdf") == 0)
+            return "application/pdf";
+        if (strcmp(last_dot, ".svg") == 0)
+            return "image/svg+xml";
+        if (strcmp(last_dot, ".txt") == 0)
+            return "text/plain";
+    }
+
+    return "application/octet-stream";
 }
 
 void sigchld_handler(int s)
@@ -48,39 +93,146 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-void received(int new_fd, char *buf, int size)
+void send_400(int socket)
 {
-    int count = 0;
-    for (int i = 0; i < size; i++) {
-        if(buf[i] == '\0') {
-            count++;
+    const char *c400 = "HTTP/1.1 400 Bad Request\r\n"
+                       "Connection: close\r\n"
+                       "Content-Length: 11\r\n\r\nBad Request";
+    send(socket, c400, strlen(c400), 0);
+    drop_client(socket);
+}
+
+void send_404(int socket)
+{
+    const char *c404 = "HTTP/1.1 404 Not Found\r\n"
+                       "Connection: close\r\n"
+                       "Content-Length: 9\r\n\r\nPage Not Found";
+    send(socket, c404, strlen(c404), 0);
+    drop_client(socket);
+}
+
+void serve(int socket, const char *path)
+{
+    printf("Serving path %s\n", path);
+
+    if (strcmp(path, "/") == 0)
+    {
+        path = "/index.html";
+    }
+    if (strlen(path) > 100)
+    {
+        // send_400(socket);
+        return;
+    }
+    if (strstr(path, ".."))
+    {
+        // send_400(socket);
+        return;
+    }
+    char full_path[128];
+
+    sprintf(full_path, "public%s", path);
+
+    FILE *fp = fopen(full_path, "rb");
+
+    if (!fp)
+    {
+        // send_404(socket);
+        return;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    size_t cl = ftell(fp);
+    rewind(fp);
+
+    const char *ct = get_content_type(full_path);
+
+    char buffer[BSIZE];
+
+    sprintf(buffer, "HTTP/1.1 200 OK\r\n");
+    send(socket, buffer, strlen(buffer), 0);
+
+    sprintf(buffer, "Connection: close\r\n");
+    send(socket, buffer, strlen(buffer), 0);
+
+    sprintf(buffer, "Content-Length: %zu\r\n", cl);
+    send(socket, buffer, strlen(buffer), 0);
+
+    sprintf(buffer, "Content-Type: %s\r\n", ct);
+    send(socket, buffer, strlen(buffer), 0);
+
+    sprintf(buffer, "\r\n");
+    send(socket, buffer, strlen(buffer), 0);
+
+    int r = fread(buffer, 1, BSIZE, fp);
+    while (r)
+    {
+        send(socket, buffer, r, 0);
+        r = fread(buffer, 1, BSIZE, fp);
+    }
+    fclose(fp);
+}
+
+void received(int new_fd, int numbytes, char *buf)
+{
+    int client_received = 0;
+    char *get = "GET /";
+    char *post = "POST /";
+    if (numbytes < 1)
+    {
+        fprintf(stderr, "Unexpected disconnect from client.\n");
+        drop_client(new_fd);
+    }
+    else
+    {
+        client_received += numbytes;
+        buf[client_received] = 0;
+
+        char *request = strstr(buf, "\r\n\r\n");
+
+        if (request)
+        {
+            *request = 0;
+
+            if (strncmp(get, buf, strlen(get-1)))
+            {
+                send_400(new_fd);
+            }
+            else if (strncmp(post, buf, strlen(post-1)))
+            {
+                send_400(new_fd);
+            }
+            else
+            {
+                char *path = buf + 4;
+                char *end_path = strstr(path, " ");
+                if (!end_path)
+                {
+                    send_400(new_fd);
+                }
+                else
+                {
+                    *end_path = 0;
+                    serve(new_fd, path);
+                }
+            }
         }
     }
-    printf("server: received '%s' with size of %d and empty bytes amount been %d\n", buf, size, count);
 }
 
-
-void send_message(int new_fd, char *buf, int size)
-{
-    if (send(new_fd, buf, size, 0) == -1)
-        perror("send");
-
-    printf("sent %s to connection %d\n", buf, new_fd);
-}
-
-void accept_connection(int sockfd)
+void manage_connection(int sockfd)
 {
 
     struct sockaddr_storage their_addr; // connector's address information
     socklen_t sin_size;
     int new_fd, numbytes;
     char s[INET6_ADDRSTRLEN];
-    char buf[MAXDATASIZE];
+    char buf[MAXDATASIZE + 1];
 
     printf("server: waiting for connections...\n");
 
     while (1)
-    { // main accept() loop
+    {
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
         if (new_fd == -1)
@@ -88,16 +240,16 @@ void accept_connection(int sockfd)
             perror("accept");
             continue;
         }
-        // todo will leave in for now, but will remove later basically just debugging
+
         inet_ntop(their_addr.ss_family,
                   get_in_addr((struct sockaddr *)&their_addr),
                   s, sizeof s);
-        printf("server: got connection from %s\n", s);
+        printf("server: got connection from %s\n\n", s);
 
-        // end
         if (!fork())
-        {                  // this is the child process
-            close(sockfd); // child doesn't need the listener
+        {
+            close(sockfd);
+
             if ((numbytes = recv(new_fd, buf, sizeof buf, 0)) == -1)
             {
                 perror("recv");
@@ -105,11 +257,8 @@ void accept_connection(int sockfd)
             }
             else
             {
-                received(new_fd, buf, sizeof buf);
+                received(new_fd, numbytes, buf);
             }
-
-            send_message(new_fd, "Hello, world!", sizeof "Hello, world!");
-            
             close(new_fd);
             exit(0);
         }
@@ -117,11 +266,12 @@ void accept_connection(int sockfd)
     }
 }
 
-void bind_socket(struct addrinfo *servinfo)
+SOCKET bind_socket(struct addrinfo *servinfo)
 {
+    // todo change all exit to return so that we can handle errors better
     struct addrinfo *p;
     struct sigaction sa;
-    int sockfd;
+    SOCKET sockfd;
     int yes = 1;
     for (p = servinfo; p != NULL; p = p->ai_next)
     {
@@ -146,10 +296,10 @@ void bind_socket(struct addrinfo *servinfo)
             continue;
         }
 
+        freeaddrinfo(servinfo);
+
         break;
     }
-
-    freeaddrinfo(servinfo);
 
     if (p == NULL)
     {
@@ -172,10 +322,10 @@ void bind_socket(struct addrinfo *servinfo)
         exit(1);
     }
 
-    accept_connection(sockfd);
+    return sockfd;
 }
 
-void get_addr_ip(char *port)
+struct addrinfo *bind_to_port(char *port)
 {
     int status;
     struct addrinfo hints, *res;
@@ -190,7 +340,7 @@ void get_addr_ip(char *port)
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         exit(EXIT_FAILURE);
     }
-    bind_socket(res);
+    return res;
 }
 
 int main(int argc, char *argv[])
@@ -200,7 +350,9 @@ int main(int argc, char *argv[])
         usage();
     }
 
-    get_addr_ip(argv[1]);
+    SOCKET socket = bind_socket(bind_to_port(argv[1]));
+
+    manage_connection(socket);
 
     return 0;
 }
