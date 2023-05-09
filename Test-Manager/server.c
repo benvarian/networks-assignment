a4e9a6e67007a076a5e4c2e350bdc87194a83e90
benvarian <88700858+benvarian@users.noreply.github.com>
2023-05-09 12:45:43 +0800

    incorporate data structures

--- Test-Manager/server.c
+++ Test-Manager/server.c
@@ -4,6 +4,118 @@
 **/
 
 #include "server.h"
+#include "Data-Structures/Queue/Queue.h"
+
+// Parses out the request line to retrieve the method, uri, and http version.
+void extract_request_line_fields(struct HTTPRequest *request, char *request_line)
+{
+    // Copy the string literal into a local instance.
+    char fields[strlen(request_line)];
+    strcpy(fields, request_line);
+    // Separate the string on spaces for each section.
+    char *method = strtok(fields, " ");
+    char *uri = strtok(NULL, " ");
+    char *http_version = strtok(NULL, "\0");
+    // Insert the results into the request object as a dictionary.
+    struct Dictionary request_line_dict = dictionary_constructor((compare_string_keys));
+    request_line_dict.insert(&request_line_dict, "method", sizeof("method"), method, sizeof(char[strlen(method)]));
+    request_line_dict.insert(&request_line_dict, "uri", sizeof("uri"), uri, sizeof(char[strlen(uri)]));
+    request_line_dict.insert(&request_line_dict, "http_version", sizeof("http_version"), http_version, sizeof(char[strlen(http_version)]));
+    // Save the dictionary to the request object.
+    request->request_line = request_line_dict;
+    if (request->request_line.search(&request->request_line, "GET", sizeof("GET")))
+    {
+        extract_body(request, (char *)request->request_line.search(&request->request_line, "uri", sizeof("uri")));
+    }
+}
+
+// Parses out the header fields.
+void extract_header_fields(struct HTTPRequest *request, char *header_fields)
+{
+    // Copy the string literal into a local instance.
+    char fields[strlen(header_fields)];
+    strcpy(fields, header_fields);
+    // Save each line of the input into a queue.
+    struct Queue headers = queue_constructor();
+    char *field = strtok(fields, "\n");
+    while (field)
+    {
+        headers.push(&headers, field, sizeof(char[strlen(field)]));
+        field = strtok(NULL, "\n");
+    }
+    // Initialize the request's header_fields dictionary.
+    request->header_fields = dictionary_constructor(compare_string_keys);
+    // Use the queue to further extract key value pairs.
+    char *header = (char *)headers.peek(&headers);
+    while (header)
+    {
+        char *key = strtok(header, ":");
+        char *value = strtok(NULL, "\0");
+        if (value)
+        {
+            // Remove leading white spaces.
+            if (value[0] == ' ')
+            {
+                value++;
+            }
+            // Push the key value pairs into the request's header_fields dictionary.
+            request->header_fields.insert(&request->header_fields, key, sizeof(char[strlen(key)]), value, sizeof(char[strlen(value)]));
+            // Collect the next field from the queue.
+        }
+        headers.pop(&headers);
+        header = (char *)headers.peek(&headers);
+    }
+    // Destroy the queue.
+    queue_destructor(&headers);
+}
+
+// Parses the body according to the content type specified in the header fields.
+void extract_body(struct HTTPRequest *request, char *body)
+{
+    // Check what content type needs to be parsed
+    char *content_type = (char *)request->header_fields.search(&request->header_fields, "Content-Type", sizeof("Content-Type"));
+    if (content_type)
+    {
+        // Initialize the body_fields dictionary.
+        struct Dictionary body_fields = dictionary_constructor(compare_string_keys);
+        if (strcmp(content_type, "application/x-www-form-urlencoded") == 0)
+        {
+            // Collect each key value pair as a set and store them in a queue.
+            struct Queue fields = queue_constructor();
+            char *field = strtok(body, "&");
+            while (field)
+            {
+                fields.push(&fields, field, sizeof(char[strlen(field)]));
+            }
+            // Iterate over the queue to further separate keys from values.
+            field = fields.peek(&fields);
+            while (field)
+            {
+                char *key = strtok(field, "=");
+                char *value = strtok(NULL, "\0");
+                // Remove unnecessary leading white space.
+                if (value[0] == ' ')
+                {
+                    value++;
+                }
+                // Insert the key value pair into the dictionary.
+                body_fields.insert(&body_fields, key, sizeof(char[strlen(key)]), value, sizeof(char[strlen(value)]));
+                // Collect the next item in the queue.
+                fields.pop(&fields);
+                field = fields.peek(&fields);
+            }
+            // Destroy the queue.
+            queue_destructor(&fields);
+        }
+        else
+        {
+            // Save the data as a single key value pair.
+            body_fields.insert(&body_fields, "data", sizeof("data"), body, sizeof(char[strlen(body)]));
+        }
+        // Set the request's body dictionary.
+        request->body = body_fields;
+    }
+}
 
 void usage(void)
 {
@@ -52,6 +164,13 @@ const char *get_content_type(const char *path)
     }
 
     return "application/octet-stream";
+}
+
+void http_request_destructor(HTTPRequest *request)
+{
+    dictionary_destructor(&request->request_line);
+    dictionary_destructor(&request->header_fields);
+    dictionary_destructor(&request->body);
 }
 
 void sigchld_handler(int s)
@@ -164,30 +283,26 @@ void connection_get(SOCKET socket, const char *path, const char *IPv6_Address)
     fclose(fp);
 }
 
-void connection_post(int socket, char *buf, char *args)
+HTTPRequest connection_post(int socket, char *buf, char *args)
 {
-    char *line;
-    int index = 0;
+    HTTPRequest response;
+    char request[strlen(buf)];
+    strcpy(request, buf);
     (void)socket;
-    (void)buf;
-    (void)args;
-    while ((line = strtok_r(buf + index, "\r\n", &line)))
+    for (int i = 0; i < (int)strlen(request) - 2; i++)
     {
-        index = (line - buf) + strlen(line) + 2;
-        char *key = NULL;
-        char *value = NULL;
-        if (strcmp(line, "") == 0)
-            break;
-        char *colon = strchr(line, ':');
-        if (colon != NULL)
+        if (request[i] == '\n' && request[i + 1] == '\n')
         {
-            value = colon + 2;
+            request[i + 1] = '|';
         }
-        
-        // sscanf(line, "%s: %s", key, value);
-        if (value != NULL)
-            printf("%s:%s\n\n", key,value);
     }
+    char *request_line = strtok(request, "\r\n");
+    char *header_fields = strtok(NULL, "|");
+    extract_header_fields(&response, header_fields);
+    extract_request_line_fields(&response, request_line);
+    extract_body(&response, args);
+
+    return response;
 }
 
 void received(int new_fd, int numbytes, char *buf, const char *IPv6_Address)
@@ -202,11 +317,11 @@ void received(int new_fd, int numbytes, char *buf, const char *IPv6_Address)
     {
         client_received += numbytes;
         buf[client_received] = 0;
-        char *request = strstr(buf, "\r\n\r\n");
+        char *res = strstr(buf, "\r\n\r\n");
 
-        if (request)
+        if (res)
         {
-            *request = 0;
+            *res = 0;
             if (strncmp(buf, "GET", 3) == 0)
             {
                 char *path = buf + 4;
@@ -224,7 +339,7 @@ void received(int new_fd, int numbytes, char *buf, const char *IPv6_Address)
             }
             else if (strncmp(buf, "POST", 4) == 0)
             {
-                connection_post(new_fd, buf, request + 4);
+                connection_post(new_fd, buf, res + 4);
             }
             else
             {
--- Test-Manager/server.h
+++ Test-Manager/server.h
@@ -11,23 +11,44 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+#include "Data-Structures/Dictionary/Dictionary.h"
+
 #define BACKLOG 10
 #define MAXDATASIZE 4095
 #define BSIZE 1024
 #define SOCKET int
+
+enum HTTPMethods
+{
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    PATCH,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE
+};
+
+typedef struct HTTPRequest {
+    struct Dictionary request_line;
+    struct Dictionary header_fields;
+    struct Dictionary body;
+} HTTPRequest;
 
 // define functions
 void usage(void);
 
 void drop_client(SOCKET socket);
 
-const char *get_content_type(const char *path); 
+const char *get_content_type(const char *path);
 
-void sigchld_handler(int s); 
+void sigchld_handler(int s);
 
 void *get_in_addr(struct sockaddr *sa);
 
-void send_200(SOCKET socket); 
+void send_200(SOCKET socket);
 
 void send_400(SOCKET socket);
 
@@ -35,7 +56,7 @@ void send_404(SOCKET socket);
 
 void connection_get(SOCKET socket, const char *path, const char *IPv6_Address);
 
-void connection_post(int socket, char *buf, char *args);
+HTTPRequest connection_post(int socket, char *buf, char *args);
 
 void received(int new_fd, int numbytes, char *buf, const char *IPv6_Address);
 
@@ -44,3 +65,9 @@ void manage_connection(SOCKET sockfd);
 SOCKET bind_socket(struct addrinfo *servinfo);
 
 struct addrinfo *get_info(char *port);
+
+void http_request_destructor(HTTPRequest *request);
+
+void extract_request_line_fields(HTTPRequest *request, char *request_line);
+void extract_header_fields(HTTPRequest *request, char *header_fields);
+void extract_body(HTTPRequest *request, char *body);
