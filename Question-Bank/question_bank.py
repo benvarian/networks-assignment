f08944e9b3857bff2192d0922b1e44119ec77fbc
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-05-14 05:22:29 +0800

    fixed multi choice marking

--- Question-Bank/question_bank.py
+++ Question-Bank/question_bank.py
@@ -24,8 +24,7 @@ class QB_question_database:
         return self.questions[qid]
 
     def get_random_qs(self, q_num):
-        # returns q_num questions as json array.
-
+        # returns q_num questions as json array. -- unsure if json is the play for now.
         qids = self.questions.keys()
         rand_ids = random.sample(list(qids), q_num)
         rand_qs = [self.questions[id] for id in rand_ids]
@@ -37,12 +36,15 @@ class QB_question_database:
         q_obj = self.get_q_by_id(qid)
         # programming
         if (q_obj[0] == 'P'):
+            # MITCH WORK
             return execute_script(q_obj[0], q_obj[1])
         elif(q_obj[0] == 'M'):
-            # multichoice
-            return q_obj[2][0] == ans
-        # raise error...
+            return 1 if q_obj[2] == ans else 0
+        # raise error...?
         return
+    
+    # TODO: send answer
 
+# OTHER MITCH WORK
 def execute_script(type, script):
     return 0
