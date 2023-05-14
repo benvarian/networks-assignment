d10938a377bb1fa910ea08f6e56e3dc12fdd2ac0
B00mStream <57031880+B00mStream@users.noreply.github.com>
2023-05-14 20:46:21 +0800

    run_code.py

--- Question-Bank/run_code.py
+++ Question-Bank/run_code.py
@@ -1,8 +1,34 @@
+import os
+from ctypes import cdll
+from distutils.ccompiler import new_compiler
+import subprocess
+
 def execute_script(lang, script):
     if(lang == "P"):
         exec(script)
-        result = locals().get(script)
+        result = locals().get('function')
         return result()
+    elif (lang == "C"):
+        main =  """\n\nint main() {\nchar *result = function();\nprintf("%s", result);\nreturn 0;}"""
+        try:
+            compiler = new_compiler()
+            with open('temp.c', 'w+') as file:
+                file.write(script)
+                file.write(main)
+            compiler.compile(['temp.c'])
+            compiler.link_shared_object(['temp.o'], 'temp.so')
+            main = cdll.LoadLibrary("./temp.so")
+            subprocess.run(['ls', '-al']) # Just printing out the directory to see if objects are created
+            print(main.function())
+            result = main.function()
+            return result
+        finally:
+            os.remove('temp.c')
+            os.remove('temp.so')
+            print("done")
+    else: return("Error: Invalid Programming Language")
 
-result = execute_script("P", """def test():\n    return("Hello World")""")
+result = execute_script("P", """def function():\n\treturn ("Hello World")""")
+print(result)
+result = execute_script("C", """#include<stdio.h>\nchar *function() {return("Hello World");}""")
 print(result)
