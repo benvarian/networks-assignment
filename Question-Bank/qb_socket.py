eaea04a795d2fd409b2f0e83a86bf1c134be3b04
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-05-17 14:15:30 +0800

    Answer response implemented

--- Question-Bank/qb_socket.py
+++ Question-Bank/qb_socket.py
@@ -37,7 +37,7 @@ QTYPES = ['C', 'P']
 MARK_HEADER = "MARK\r\n"
 QUESTION_HEADER = "QUESTIONS\r\n"
 ERROR_HEADER = "ERROR\r\n"
-
+ANSWER_HEADER = "ANSWER\r\n"
 END_HEADER = "\r\n"
 
 END_MSG = "\r\n\0"
@@ -117,6 +117,19 @@ class Nick_Socket:
         sent = self.sock.send(byte_msg)
         if sent == 0:
             raise RuntimeError("Socket Connection Broken")
+        
+        
+    # sends string along sock
+    def send_answer(self, msg):
+        msg = QB_HEADER + ANSWER_HEADER + END_HEADER + msg + END_MSG
+        MSGLEN = len(msg)
+        byte_msg = msg.encode()
+
+        print("\nsending questions:\nMSGLEN:",  MSGLEN, "\nmsg:", msg, "\n")
+
+        sent = self.sock.send(byte_msg)
+        if sent == 0:
+            raise RuntimeError("Socket Connection Broken")
 
     def send_mark(self, mark):
         # M for mark
@@ -187,10 +200,11 @@ class Nick_Socket:
                 return
             questions = ''.join(["qid:{}&type:{}&question:{}&".format(q[0], q[1], q[2]) for q in QB_DB.get_rand_qs(int(q_num))])[:-1]
             self.send_questions(questions)
-        elif(mode_req == "QUESTION\r\n"):
+        elif(mode_req == ANSWER_HEADER):
             qid = int(msg[1])
             question = question_bank.get_q_by_id(qid)
-            self.send_questions(question)
+            answer = question[2]
+            self.send_answer(answer)
         # handle pings from tm just by making an elif as its a viable header 
         elif (mode_req == "TM\r\n"):
             print("TM PINGED QB Responding with ACCEPTED PING")
